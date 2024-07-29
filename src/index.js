1487efc4befa9f8639fcdf2a8d042aacb852f39c
Наташка Овечкина <natallia.ovechkina@gmail.com>
2024-07-29 22:31:23 +0300

    init: index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
-}
+    let newNumb = Math.abs(n).toString();
+    const tmp = newNumb.split('');
+    tmp.reverse();
+    newNumb=tmp.join('');
+    return +newNumb;
+  }
